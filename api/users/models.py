d3b2b71e279c54bb2ebf7b3c7db5492d94d96a5c
ddc <ddc@users.noreply.github.com>
2025-03-14 16:59:56 -0300

    first commit

--- api/users/models.py
+++ api/users/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from api.db import Base
 
@@ -13,3 +14,15 @@ class Users(Base):
     last_login: Mapped[str] = mapped_column(nullable=True)
     first_name: Mapped[str] = mapped_column(nullable=False)
     last_name: Mapped[str] = mapped_column(nullable=False)
+    student = relationship("Students", back_populates="user")
+
+
+class Students(Base):
+    __tablename__ = "students"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    enrollment_date: Mapped[str] = mapped_column(nullable=False)
+    min_course_credits: Mapped[int] = mapped_column(nullable=False)
+    first_name: Mapped[str] = mapped_column(nullable=False)
+    last_name: Mapped[str] = mapped_column(nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey(Users.id, ondelete="CASCADE"))
+    user = relationship("Users", back_populates="student")
--- api/users/routes.py
+++ api/users/routes.py
@@ -1,18 +1,20 @@
 from typing import Optional
-
+from flask import request, jsonify
 from flask_openapi3 import APIBlueprint
 from pydantic import BaseModel
 from sqlalchemy import select
 
 from api.users.models import Users
+from api.users.models import Students
 from database import db
 
 
 users_app = APIBlueprint("users_app", __name__)
+students_app = APIBlueprint("students_app", __name__)
 
 
 class UserSchema(BaseModel):
-    id: Optional[int]
+    id: int
     password: str
     email: str
     created_at: str
@@ -29,6 +31,20 @@ class UserList(BaseModel):
     users: list[UserSchema]
 
 
+class StudentSchema(BaseModel):
+    id: Optional[int]
+    enrollment_date: str
+    min_course_credits: str
+    first_name: str
+    last_name: str
+
+    class Config:
+        orm_mode = True
+
+class StudentList(BaseModel):
+    students: list[StudentSchema]
+
+
 @users_app.get("/users", responses={"200": UserList})
 def get_users():
     with db.session() as session:
@@ -39,3 +55,36 @@ def get_users():
             in users_query
         ]
         return {"users": users_list}
+
+#### STUDENT
+@users_app.post("/student")
+def create_student():
+    with db.session() as session:
+        content = request.json
+        stmt = Students(
+            first_name=content["first_name"],
+            last_name=content["last_name"],
+            enrollment_date=content["enrollment_date"],
+            min_course_credits=content["min_course_credits"],
+            user_id=content["user_id"],
+        )
+        session.add(stmt)
+        session.commit()
+        return {"success": True}
+
+
+@users_app.get("/students", responses={"200": StudentList})
+def get_student():
+    with db.session() as session:
+        student_query = session.execute(select(Students)).scalars().all()
+        student_list = [ StudentSchema.from_orm(students).dict() for students in student_query ]
+        return {"users": student_list}
+
+
+@users_app.get("/student/<string:student_id>")
+def get_single_student():
+    suid = request.view_args.get("student_id")
+    with db.session() as session:
+        student_query = session.execute(select(Students).where(Students.id == suid)).scalars()
+        student_list = [ StudentSchema.from_orm(students).dict() for students in student_query ]
+        return {"users": student_list}
